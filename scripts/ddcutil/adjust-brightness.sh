8fd3b5619dfae5c0bc5967c1255aa217420fe289
Patrick Horne <patrickahorne@gmail.com>
2020-01-31 20:53:43 +1100

    Restructure projects

--- scripts/ddcutil/adjust-brightness.sh
+++ scripts/ddcutil/adjust-brightness.sh
@@ -1,12 +1,12 @@
 # Increase the brightness of display 3 by 25%
 # for specifying in the "-d" flag
 
+# Get a space-separated list of display numbers
 DISPLAYS=`sudo ddcutil detect -t \
-  | grep -i display |awk '{print $2}' \
-  | paste -sd " "`
+  |grep -i display |awk '{print $2}' \
+  |paste -sd " "`
 
-
-
+# iterate over each display
 for DISPLAY in $DISPLAYS; do
   sudo ddcutil -d $DISPLAY setvcp 10 $1
 done
