bc2e8611d3dae49b31aeb2a2e7611d3a5288349b
Patrick Horne <patrickahorne@gmail.com>
2020-02-02 20:58:46 +1100

    Add untouched Consolas font

--- deploy/windows/symlink-config.ps1
+++ deploy/windows/symlink-config.ps1
@@ -2,7 +2,8 @@ New
 # symlink neovim dotfile
 $source = "../../src/
 $target = $env:appdata + '/alacritty/alacritty.yml'
-cmd /c mklink /d "../../src/universal/neovim/syntax" 
+
 
 # symlink 
-New-Item -Path 
+cmd /c mklink /d "../../src/universal/neovim/syntax" 
+
